0d39e79110f10bcb6c42c1e142776b05b407db76
Junmo Park <knight2683@gmail.com>
2022-02-14 07:10:41 +0000

    inference code

--- inference.py
+++ inference.py
@@ -2,6 +2,7 @@ import sys, os
 import argparse
 sys.path.append('src')
 import torch
+from rouge import Rouge
 from summary_model import DeletionBasedSummaryModel
 from data_utils import get_dataloader
 from tqdm import tqdm
@@ -10,7 +11,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def inference(args):
     model_name = 'intent_class_model.pt'
-    model_path = 'koelectra_small_ckpt/checkpoint-1500'
+    model_path = 'klue_roberta_large_ckpt/checkpoint-24000'
     model = torch.load(os.path.join(model_path, model_name))
     tokenizer = model.tokenizer    
 
@@ -18,23 +19,39 @@ def inference(args):
 
     target_list = []
     infer_list = []
-    for batch in tqdm(test_dataloader):
-        inputs, labels, target_text = batch
-        inputs, labels = inputs.to(device), labels.to(device)
-        input_ids, attention_mask, token_type_ids = inputs['input_ids'], inputs['attention_mask'], inputs['token_type_ids']
+    with torch.no_grad():
+        for batch in tqdm(test_dataloader):
+            inputs, labels, target_text = batch
+            inputs, labels = inputs.to(device), labels.to(device)
+            input_ids, attention_mask, token_type_ids = inputs['input_ids'], inputs['attention_mask'], inputs['token_type_ids']
 
-        outputs = model(input_ids, attention_mask, token_type_ids)
-        preds = (outputs.squeeze(-1) > 0.5)
+            outputs = model(input_ids, attention_mask, token_type_ids)
+            preds = (outputs.squeeze(-1) > 0.5)
 
-        preds_ids = (preds*input_ids).tolist()
-        preds_tokens = tokenizer.batch_decode(preds_ids, skip_special_tokens=True)
+            preds_ids = (preds*input_ids).tolist()
+            preds_tokens = tokenizer.batch_decode(preds_ids, skip_special_tokens=True)
 
-        target_list += target_text
-        infer_list += preds_tokens
-        # tokenizer.batch_decode((labels.type(torch.long)*input_ids).tolist(), skip_special_tokens=True)
-        
-        print(1)
-        
+            target_list += target_text
+            infer_list += preds_tokens
+
+    with open('./infer_result.txt', 'w', encoding='utf-8') as f:
+        f.write('\n'.join(infer_list))
+    with open('./target_result.txt', 'w', encoding='utf-8') as f:
+        f.write('\n'.join(target_list))
+    get_rouge(target_list, infer_list, tokenizer)
+
+
+def get_rouge(target_list, infer_list, tokenizer):
+    target_list = [' '.join(tokenizer.tokenize(_)) for _ in target_list]
+    temp_infer_list = []
+    for _ in infer_list:
+        if len(_) <= 1:
+            _ = '비어있음'
+        temp_infer_list.append(' '.join(tokenizer.tokenize(_)))
+    infer_list = temp_infer_list
+    scorer = Rouge()
+    print(scorer.get_scores(infer_list, target_list, avg=True))
+
 
 
 if __name__ == '__main__':
